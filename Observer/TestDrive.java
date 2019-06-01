897b32c0ae36bebf223a7b15ce704e380e8de11b
Jose Ricardo <josericardo29@hotmail.es>
2019-05-31 22:36:41 -0400

    Observable and Observer API Implemented.

--- Observer/TestDrive.java
+++ Observer/TestDrive.java
@@ -1,5 +1,11 @@
+import java.util.Observer;
+import java.util.Observable;
 public class TestDrive {
     public static void main(String[] args) {
+        Observable weatherData = new WeatherData();
         
+        Observer currentConditionsDisp = new CurrentConditionsDisplay(new WeatherData());
+        weatherData.addObserver(currentConditionsDisp);
+        ((WeatherData) weatherData).setMeasurements(120f, 22f, 10f);
     }
 }
