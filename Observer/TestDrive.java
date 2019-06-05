f5f797ae3173d9be2e98efd562500f9d0ec4f72a
Jose Ricardo <josericardo29@hotmail.es>
2019-06-05 03:53:19 -0400

    Wicket Package Just Added

--- Observer/TestDrive.java
+++ Observer/TestDrive.java
@@ -3,8 +3,7 @@ import java.util.Observable;
 public class TestDrive {
     public static void main(String[] args) {
         Observable weatherData = new WeatherData();
-        
-        Observer currentConditionsDisp = new CurrentConditionsDisplay(new WeatherData());
+        Observer currentConditionsDisp = new CurrentConditionsDisplay(weatherData);
         weatherData.addObserver(currentConditionsDisp);
         weatherData.addObserver(new ForeCastDisplay(weatherData));
         ((WeatherData) weatherData).setMeasurements(120f, 22f, 10f);
