8de983638dad1af2eb950b294a7384c0a91e9f06
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-06-06 13:01:02 -0400

    More Examples Done

--- IO_NIO/IO_NIO/src/main/java/org/io/research/App.java
+++ IO_NIO/IO_NIO/src/main/java/org/io/research/App.java
@@ -1,19 +1,25 @@
 package org.io.research;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
 
-public class App 
-{
-    public static void main( String[] args )
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException
     {
         fileExample();
         fileWriterAndReaderExample();
         fileWithBytes();
+        filesAndDirs();
+    
     }
 
     private static void fileExample() {
@@ -64,6 +70,34 @@ public class App
             for(byte c : in)
                 System.out.print((char)c);
             fi.close();
-        } catch(Exception e) { e.printStackTrace();}
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    public static void filesAndDirs() {
+        try {
+            File books = new File("books");
+            books.mkdir();
+            if (books.isDirectory()) {
+                Logger.getLogger(App.class.getName()).info(books.getName() + " is my directory");                
+            }
+            File bookTitles = new File(books, "BooksTitle.txt");
+            bookTitles.createNewFile();
+            if (bookTitles.isFile()) {
+                Logger.getLogger(App.class.getName()).info(bookTitles.getName() + " is my file");
+            }
+
+            PrintWriter writer = new PrintWriter(new FileWriter(bookTitles));
+            writer.println("1- El Coronel no tiene quien le escriba");
+            writer.println("2 - Steve Jobs ");
+            writer.flush();
+            writer.close();
+            
+            BufferedReader reader = new BufferedReader(new FileReader(bookTitles));
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) { e.printStackTrace(); }
     }
 }
