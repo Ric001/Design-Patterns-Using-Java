3b322dde2ac82395c8a0a7c72952a5d89642dbec
Jose Ricardo <josericardo29@hotmail.es>
2019-06-18 19:44:32 -0400

    At this point we succesfully implemented a link counter using both normal requests and ajax calls and forms used to retrieve and display informations on screen as well. Im so Proud and Excited of coding. To the next time.

---
+++ Wicket/myproject/src/main/java/com/mycompany/Another.java
@@ -0,0 +1,44 @@
+package com.mycompany;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class Another extends WebPage {
+    private static final long serialVersionUID = 1L;
+    private Label label;
+    private TextField<String> field;
+
+    public Another(final PageParameters parameters) {
+        Form<String> form = new Form<>("form");
+        
+        form.add(field = new TextField<>("field", new Model<>("")))
+            .add(new Button("button") {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onSubmit() {
+                String value = (String) field.getModelObject();
+                label.setDefaultModelObject(value);
+                field.setDefaultModelObject("");
+            }
+        });
+        
+        label = new Label("message");
+        label.setDefaultModel(new Model<>(""));
+        add(label)
+        .add(form);
+    }
+
+    public TextField<String> getTextField() {
+        return field;
+    }
+
+    public void setTextField(TextField<String> field) {
+        this.field = field;
+    }
+}
--- Wicket/myproject/src/main/java/com/mycompany/HomePage.java
+++ Wicket/myproject/src/main/java/com/mycompany/HomePage.java
@@ -1,26 +1,50 @@
 package com.mycompany;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	private int counter = 0;
+	private Label label;
+	private Label toTheOtherPage;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-		 add(new Label("version", getApplication().getFrameworkSettings().getVersion()))
-		.add(new Link<Void>("Link") {
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		label = new Label("label", new PropertyModel<Void>(this, "counter"));
+		addAjaxCounter();
+		addAnotherPageLink();
+	}
+
+	public void addAjaxCounter() {
+		add(new AjaxFallbackLink<Void>("Link") {
+			private static final long serialVersionUID = 1L;
 			@Override
-			public void onClick() {
+			public void onClick(Optional<AjaxRequestTarget> optional) {
 				counter++;
+				if (Objects.nonNull(optional.get()))
+					optional.get().add(label);
 			}
-		})
-		.add(new Label("label", 
-			new PropertyModel(this, "counter")));
+		});
+	}
+
+	private void addAnotherPageLink() {
+		add(label.setOutputMarkupId(true)).add(new AjaxFallbackLink<Void>("toTheOther") {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void onClick(Optional<AjaxRequestTarget> optionalTarget) {
+				if (Objects.nonNull(optionalTarget))
+					setResponsePage(Another.class);
+			}
+		});
 	}
 
 	public int getCounter() {
@@ -30,4 +54,25 @@ public class HomePage extends WebPage {
 	public void setCounter(int counter) {
 		this.counter = counter;
 	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Label getLabel() {
+		return label;
+	}
+
+	public void setLabel(Label label) {
+		this.label = label;
+	}
+
+	public Label getToTheOtherPage() {
+		return toTheOtherPage;
+	}
+
+	public void setToTheOtherPage(Label toTheOtherPage) {
+		this.toTheOtherPage = toTheOtherPage;
+	}
+
 }
