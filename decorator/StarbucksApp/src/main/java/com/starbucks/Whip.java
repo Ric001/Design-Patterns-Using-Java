0859e806eb650b089223252a4d0749270613c86a
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-06-28 14:02:14 -0400

    New Changes Added

---
+++ decorator/StarbucksApp/src/main/java/com/starbucks/Whip.java
@@ -0,0 +1,19 @@
+package com.starbucks;
+
+public class Whip extends CondimentDecorator {
+
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    
+    @Override 
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip"; 
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .20F;
+    }
+}
