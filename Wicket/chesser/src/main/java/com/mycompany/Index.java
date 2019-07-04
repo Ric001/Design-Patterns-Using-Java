bd0ddf3ce9f167595234a5525f03158a2c26cc54
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-07-04 13:21:27 -0400

    Some Server side logic was added for the landing page

--- Wicket/chesser/src/main/java/com/mycompany/Index.java
+++ Wicket/chesser/src/main/java/com/mycompany/Index.java
@@ -1,13 +1,50 @@
 package com.mycompany;
 
-import org.apache.wicket.Page;
 
-public class Index extends Page {
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 
+import java.util.logging.Logger;
+
+public class Index extends CheeserPage {
     private static final long serialVersionUID = 1L;
+    private static final Logger _LOG = Logger.getLogger(Index.class.getName());
 
     public Index() {
-        System.console().printf("SAAAA %S", "UUUU");
+        add(new ListView<Chesse>("cheeses", getCheesses()) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void populateItem(ListItem<Chesse> item) {
+                _LOG.info("[ENTERING populateItem(ListItem<Chesse> item):void]");
+                performItemsPopulation(item);
+            }
+        });
+    }
+
+    private void performItemsPopulation(ListItem<Chesse> item) {
+        _LOG.info("[ENTERING performItemsPopulation(ListItem<Chesse> item): void]");
+        Chesse cheese = item.getModelObject();
+        item.add(new Label("name", cheese.getName()))
+            .add(new Label("description", cheese.getDescription()))
+            .add(new Label("price", cheese.getPrice()))
+
+            .add(new Link<Chesse>("add", item.getModel()) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onClick() {
+                triggerOnClick(this);
+            }
+        });
+    }
+
+    private void triggerOnClick(Link<Chesse> link) {
+        _LOG.info("[Adding selected Chesse to the cart]");
+        Chesse selected = link.getModelObject();
+        getCart().getChesses().add(selected);
     }
 
 }
