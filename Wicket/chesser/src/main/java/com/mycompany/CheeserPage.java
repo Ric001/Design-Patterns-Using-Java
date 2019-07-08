5af180dffe519d2b32c9cac612c2500f0d69a009
Jose Ricardo <josericardo29@hotmail.es>
2019-07-08 19:05:46 -0400

    CheeserPage Changes added

--- Wicket/chesser/src/main/java/com/mycompany/CheeserPage.java
+++ Wicket/chesser/src/main/java/com/mycompany/CheeserPage.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.wicket.markup.html.WebPage;
 
 public abstract class CheeserPage extends WebPage {
+    private static final long serialVersionUID = 1L;
+
     public CheeserSession getCheeserSession() {
         return (CheeserSession) getSession();
     }
