9e6cbd935f0aed67bb3c70b18a8c2695143a7732
Jose Ricardo <josericardo29@hotmail.es>
2019-09-10 00:06:28 -0400

    Detachable model clone coded. Test still needed

---
+++ Wicket/test-project/src/main/java/com/mycompany/models/Cheese.java
@@ -0,0 +1,50 @@
+package com.mycompany.models;
+
+public class Cheese {
+    
+    private Long id;
+    private String name;
+    private float price;
+
+    public Cheese(Long id, String name, float price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+    public Cheese() {
+	}
+
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Cheese [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+
+	
+
+    
+}
---
+++ Wicket/test-project/src/main/java/com/mycompany/wrappers/CheeseModel.java
@@ -0,0 +1,44 @@
+package com.mycompany.wrappers;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.mycompany.models.Cheese;
+
+import org.apache.wicket.model.Model;
+
+public class CheeseModel extends Model {
+
+    private static final long serialVersionUID = 1L;
+    
+    private transient Cheese chesse;
+    
+    private Long id;
+
+    public CheeseModel() {
+    }
+
+    public Cheese getObjectModel() {
+        if (Objects.nonNull(chesse))
+            return chesse;
+        if (Objects.isNull(chesse))
+            return new Cheese();
+        return dao(id);
+    }
+
+    public Cheese dao(final Long id) {
+        Cheese cheese = new Cheese();
+        cheese.setId(id);  
+        return new Cheese();
+    }
+    public void setObject(Object object) {
+        this.chesse = (Cheese) object;
+        this.id = (Objects.isNull(chesse)) ? null : this.chesse.getId();
+    }
+
+   @Override
+   public void detach() {
+       this.chesse = null;
+   }
+
+}
