b98012a9aa92c79d4ae16f93e61f8791fae3d83c
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-09-11 17:07:40 -0400

    Last Methods of the Array List class covered by the OCA Manual on proccess

--- CommonsOCA/src/main/java/org/commons/oca/App.java
+++ CommonsOCA/src/main/java/org/commons/oca/App.java
@@ -1,33 +1,23 @@
 package org.commons.oca;
 
 import java.util.ArrayList;
-import java.util.logging.Logger;
+import java.util.List;
 
 public class App 
 {
-    private final static Logger LOG = Logger.getLogger(App.class.getName());
+
     public static void main( String[] args )
     {
-        LOG.info("[ENTERING void main(String[] args)]");
+        final List<Product> products = new ArrayList<>();
+        final Product product = new Product("JAMON");
+        final Product p2 = new Product("JABON");
+        products.add(product);
+        products.add(p2);
 
-       final ArrayList<MyPerson> persons = new ArrayList<>();
-
-       final MyPerson p1 = new MyPerson("Freya");
-       final MyPerson p2 = new MyPerson("Paul");
-       final MyPerson p3 = new MyPerson("Harry");
         
-        persons.add(p1);
-        persons.add(p2);
-        persons.add(p3);
-        persons.remove(new MyPerson("Paul"));
-
-        for (MyPerson e : persons) 
-            System.out.println(e.getName());
-
-        final ArrayList<MyPerson> strings = new ArrayList<>();
-        strings.addAll(persons);
         
-        LOG.info("[ENDING void mani(String[] args)]");
     }
+
+
 
 }
--- CommonsOCA/src/main/java/org/commons/oca/MyPerson.java
+++ CommonsOCA/src/main/java/org/commons/oca/MyPerson.java
@@ -1,5 +1,7 @@
 package org.commons.oca;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class MyPerson {
@@ -28,4 +30,100 @@ public class MyPerson {
         return name;
     }
     
+    public void managingReferences() {
+        _LOG.info("[ENTERING void managingReferences()]");
+        
+        final List<String> myArrList = new ArrayList<>();
+
+        myArrList.add("One");
+        myArrList.add("Two");
+        myArrList.set(myArrList.indexOf("One"), myArrList.get(0).replace("O", "B"));
+        
+        final List<String> yourArrList = myArrList;
+
+        for (String val : myArrList)
+            System.out.println(val);
+
+        for (String val : yourArrList)
+            System.out.println(val);
+        
+        _LOG.info("[ENDING void managingReferences()]");
+    }
+
+    public void clearing() {
+        _LOG.info("[ENTERING  clearing(): void]");
+        
+        final ArrayList<String> myArrlist = new ArrayList<>();
+        myArrlist.add("One");
+        myArrlist.add("Two");
+        myArrlist.clear();
+
+        for(String val : myArrlist)
+            System.out.println(val);
+
+        _LOG.info("[ENDING clearing(): void]");
+    }
+
+    public void gettingAndSize() {
+        _LOG.info("[ENTERING void gettingAndSize()]");
+        
+        final List<String> myArrList = new ArrayList<>();
+        myArrList.add("One");
+        myArrList.add("Two");
+
+        final String valFormList = myArrList.get(1);
+        System.out.println(valFormList);
+        System.out.println(myArrList.size());
+
+        _LOG.info("[ENDING void gettingAndSize()]");
+    }
+    
+    public void afterWinter() {
+        _LOG.info("[ ENTERING void afterWinter() ]");
+
+        final ArrayList<StringBuilder> mArrayList = new ArrayList<>();
+        final StringBuilder sb = new StringBuilder("Jan");
+        final StringBuilder sb2 = new StringBuilder("Feb");
+        final StringBuilder sb3 = new StringBuilder("Ma");
+        
+        mArrayList.add(sb);
+        mArrayList.add(sb2);
+        mArrayList.add(sb3);
+
+        System.out.println(mArrayList.contains(new StringBuilder("Jan")));
+        System.out.println(mArrayList.contains(sb));
+        System.out.println(mArrayList.indexOf(new StringBuilder("Feb")));
+        System.out.println(mArrayList.indexOf(sb2));
+        System.out.println(mArrayList.lastIndexOf(new StringBuilder("Feb")));
+        System.out.println(mArrayList.lastIndexOf(sb2));
+
+        _LOG.info("[ ENDING void afterWinter() ]");
+    }
+
+}
+
+class Product {
+    private String name;
+    
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (ob instanceof Product) {
+            final Product p = (Product) ob;
+            final boolean isEqual = name.equals(p.getName());
+            return isEqual;
+        }
+        return false;
+    }
 }
