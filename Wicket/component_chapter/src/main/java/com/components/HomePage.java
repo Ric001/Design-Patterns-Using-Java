52a3e17f6617a0e502c2be208c294a4282b0bc71
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-11-21 15:08:06 -0400

    New Changes Added

--- Wicket/component_chapter/src/main/java/com/components/HomePage.java
+++ Wicket/component_chapter/src/main/java/com/components/HomePage.java
@@ -1,7 +1,5 @@
 package com.components;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,14 +9,16 @@ import java.util.logging.Logger;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.CheckBoxMultipleChoice;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.ListChoice;
+import org.apache.wicket.markup.html.form.ListMultipleChoice;
 import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.TextArea;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -27,10 +27,10 @@ public class HomePage extends WebPage {
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-		final Form<Void> form = form("form");
-		// final Form<Details> detailsForm = compoundPropertyForm("cpmForm");
+		Form<Void> form = multipleChoices(form("form"));
+		form = checkBoxMultipleChoices(form);
 		add(form);
-
+		// final Form<Details> detailsForm = compoundPropertyForm("cpmForm");
 	}
 
 	private Form<Void> form(final String id) {
@@ -47,7 +47,7 @@ public class HomePage extends WebPage {
 		final DropDownChoice<String> dropDownChoice = new DropDownChoice<>("dropdown",
 				new PropertyModel<>(details, "dropChoice"), categories);
 		final RadioChoice<String> radioChoices = new RadioChoice<>("radioChoices", new PropertyModel<>(details, "radioChoice"), categories);
-		
+		radioChoices.setSuffix("<br>");		
 		details.setCategory("Blue Vein");
 		details.setDropChoice("VENUS");
 		listChoice.add(new AttributeModifier("size", categories.size()));
@@ -56,6 +56,43 @@ public class HomePage extends WebPage {
 		form.add(dropDownChoice);
 		form.add(radioChoices);
 		LOG.info("[RETURNING FROM Form<Void> form()]");
+		return form;
+	}
+
+	private Form<Void> multipleChoices(Form<Void> form) {
+		LOG.info("[ENTERING Form<Vodid> multipleChoices(Form<Void> form)]");
+
+		if (Objects.isNull(form))
+			return form;
+		
+		final List<String> choices = Arrays.asList("Camel", "Cow", "Goat", "Something");
+		final Cheese cheese = new Cheese();
+		
+		final ListMultipleChoice<String> multipleChoice = new ListMultipleChoice<>("multiple",new PropertyModel<>(cheese, "milkTypes"), choices);
+		final Link<Void> submit = new Link<Void>("send") {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void onClick() {
+				System.out.println(String.format("[ <==== CHEESE ====> %s]", multipleChoice.getDefaultModel().toString()));
+			}
+		};
+		form.add(multipleChoice);
+		form.add(submit);
+		LOG.info("[RETURNING FROM Form<Void> multipleChoices(Form<Void> form)]");
+		return form;
+	}
+
+	private Form<Void> checkBoxMultipleChoices(Form<Void> form) {
+		LOG.info("[ENTERING Form<Void> checkBoxMultipleChoices(Form<Void> form)]");
+		if (Objects.isNull(form))
+			return form;
+		
+		final List<String> choices = Arrays.asList("Camel", "Cow", "Kangaroo");
+		final Cheese cheese = new Cheese();
+		final CheckBoxMultipleChoice<String> checkBoxes = new CheckBoxMultipleChoice<>("checkboxes", new PropertyModel<>(cheese, "milkTypes"), choices);
+		form.add(checkBoxes);
+
+		LOG.info("[RETURNING FROM Form<Void> checkBoxMultipleChoices(Form<Void> form)]");
 		return form;
 	}
 
@@ -105,4 +142,28 @@ class Details implements Serializable {
 		this.category = category;
 	}
 
+}
+
+
+class Cheese implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<String> milkTypes = new ArrayList<>();
+
+	public List<String> getMilkTypes() {
+		return milkTypes;
+	}
+
+	public void setMilkTypes(List<String> milkTypes) {
+		this.milkTypes = milkTypes;
+	}
+
+	@Override
+	public String toString() {
+		return "Cheese [milkTypes=" + milkTypes + "]";
+	}
+
+	
+	
 }
--- command/Command/src/main/java/org/dp/command/App.java
+++ command/Command/src/main/java/org/dp/command/App.java
@@ -1,6 +1,12 @@
 package org.dp.command;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.dp.command.commandimpls.GarageCloseDoorCommand;
 import org.dp.command.commandimpls.GarageOpenDoorCommand;
@@ -11,21 +17,24 @@ import org.dp.command.infrastructure.ICommand;
 import org.dp.command.invoker.RemoteControl;
 import org.dp.command.models.GarageDoor;
 import org.dp.command.models.Light;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(final String[] args) {
         final RemoteControl remoteControl = new RemoteControl();
         final Light livingRoom = new Light();
         final Light kitchenRoom = new Light();
-        
+
         final LightOnCommand lightOnCommand = new LightOnCommand(livingRoom);
         final LightOffCommand lightOffCommand = new LightOffCommand(kitchenRoom);
 
         final GarageDoor door = new GarageDoor();
         final GarageOpenDoorCommand openCommand = new GarageOpenDoorCommand(door);
-        final GarageCloseDoorCommand closeCommand = new GarageCloseDoorCommand(door); 
+        final GarageCloseDoorCommand closeCommand = new GarageCloseDoorCommand(door);
 
         remoteControl.setCommand(RemoteControl.ZERO_SLOT, lightOnCommand, lightOffCommand);
         remoteControl.setCommand(RemoteControl.ONE_SLOT, openCommand, closeCommand);
@@ -35,25 +44,47 @@ public class App
 
         final ICommand offMacroCommand = new MacroCommand(Optional.of(offMacroCommandArray));
         final ICommand onMacroCommand = new MacroCommand(Optional.of(onMacroCommandArray));
-        
+
         remoteControl.setCommand(RemoteControl.TWO_SLOT, onMacroCommand, offMacroCommand);
         System.out.println(remoteControl);
 
         lights(remoteControl);
 
-        //door(remoteControl);
+        // door(remoteControl);
+
+        final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            final Document document = documentBuilder.parse(new File("response.xml"));
+            document.getDocumentElement();
+            NodeList nodeList = document.getElementsByTagName("CD");
+            for (int temp = 0; temp < nodeList.getLength(); temp++) {
+                Node node = nodeList.item(temp);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    System.out.println(element.getElementsByTagName("TITLE").item(0).getTextContent());
+                    System.out.println(element.getElementsByTagName("ARTIST").item(0).getTextContent());
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void lights(RemoteControl remoteControl) {
-        
+    private static void lights(final RemoteControl remoteControl) {
+
         remoteControl.onButtonWasPushed(RemoteControl.ZERO_SLOT);
         remoteControl.undo();
         remoteControl.offButtonWasPushed(RemoteControl.ONE_SLOT);
         remoteControl.undo();
-        
+
     }
 
-    private static void door(RemoteControl remoteControl) {
+    private static void door(final RemoteControl remoteControl) {
 
         remoteControl.onButtonWasPushed(RemoteControl.ONE_SLOT);
         remoteControl.undo();
@@ -62,7 +93,7 @@ public class App
 
     }
 
-    private static void runMacro(RemoteControl remoteControl) {
+    private static void runMacro(final RemoteControl remoteControl) {
 
         remoteControl.onButtonWasPushed(RemoteControl.TWO_SLOT);
         remoteControl.undo();
